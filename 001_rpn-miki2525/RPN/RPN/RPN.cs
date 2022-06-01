d8b25fdae19bd11851baa43de9459f20dfcfa23e
miki2525 <56227439+miki2525@users.noreply.github.com>
2022-06-01 21:18:51 +0200

    RPN

---
+++ 001_rpn-miki2525/RPN/RPN/RPN.cs
@@ -0,0 +1,57 @@
+﻿using RPN.utils;
+using System;
+using System.Collections.Generic;
+
+namespace RPNCalulator {
+	public class RPN: RPNMethods
+	{
+		private readonly Stack<int> _operators;
+		private readonly Dictionary<string, Func<int, int, int>> _operationFunction;
+
+        public RPN()
+        {
+			_operationFunction = new Dictionary<string, Func<int, int, int>>
+			{
+				["+"] = (fst, snd) => fst + snd,
+				["-"] = (fst, snd) => fst - snd,
+				["*"] = (fst, snd) => fst * snd,
+				["/"] = (fst, snd) => fst / snd,
+				["!"] = (fst, snd) => countFactorial(fst)
+			};
+			_operators = new Stack<int>();
+		}
+
+		public int EvalRPN(string input) 
+		{
+			var splitInput = input.Split(' ');
+			foreach (var op in splitInput)
+			{
+
+				int valueToPush = 0;
+				if(IsNumber(op, out valueToPush))
+				{
+					_operators.Push(valueToPush);
+					continue;
+				}
+				else if (IsOperator(op))
+				{
+					var num1 = _operators.Pop();
+					var num2 = _operators.Pop();
+					_operators.Push(_operationFunction[op](num1, num2));
+				}
+                else if (IsFactorial(op))
+                {
+					var num1 = _operators.Pop();
+					_operators.Push(_operationFunction[op](num1, 0));
+				}
+			}
+
+			var result = _operators.Pop();
+			if (_operators.IsEmpty)
+			{
+				return result;
+			}
+			throw new InvalidOperationException();
+		}
+	}
+}
---
+++ 001_rpn-miki2525/RPN/RPN/utils/RPNMethods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RPN.utils
+{
+    public class RPNMethods
+    {
+
+        public int countFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * countFactorial(n-1);
+        }
+
+        public bool IsNumber(String input, out int valueToPush)
+        {
+            if (int.TryParse(input, out valueToPush))
+            {
+                return true;
+            }
+            char firstChar = (input.ToCharArray()[0]);
+            switch (firstChar)
+            {
+                case 'B':
+                    valueToPush = Convert.ToInt32(input.Substring(1), 2);
+                    return true;
+
+                case 'D':
+                    valueToPush = int.Parse(input.Substring(1));
+                    return true;
+
+                case '#':
+                    valueToPush = Convert.ToInt32(input.Substring(1), 16);
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        public bool IsOperator(String input) =>
+            input.Equals("+") || input.Equals("-") ||
+            input.Equals("*") || input.Equals("/");
+
+
+
+        public bool IsFactorial(String input) =>
+            input.Equals("!");
+        }
+}
